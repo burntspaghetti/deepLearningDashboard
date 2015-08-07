0014a5ac6941a48e999acb9c6009da4f820972c7
singularitarian <nsbertanzetti@crimson.ua.edu>
2015-08-06 21:21:56 -0500

    working on feeding twitter into alchemy

--- laravel/app/controllers/TwitterController.php
+++ laravel/app/controllers/TwitterController.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once('../vendor/j7mbo/twitter-api-php/TwitterAPIExchange.php');
+require_once '../alchemyapi_php/alchemyapi.php';
+
 class TwitterController extends BaseController {
 
 	/*
@@ -17,6 +20,71 @@ class TwitterController extends BaseController {
 
 	public function search()
 	{
+		$searchTerm = urlencode(Input::get('twitterSearch'));
+		//add checkbox for mixed, recent, or popular param
+		$result_type = 'mixed';
+		$count = 100;
+
+		
+
+		$settings = array(
+			'oauth_access_token' => Config::get('oauth.oauth_access_token'),
+			'oauth_access_token_secret' => Config::get('oauth.oauth_access_token_secret'),
+			'consumer_key' => Config::get('oauth.consumer_key'),
+			'consumer_secret' => Config::get('oauth.consumer_secret')
+		);
+
+		$url = 'https://api.twitter.com/1.1/search/tweets.json';
+
+		//two word search
+		//https://twitter.com/search?q=barack%20obama&src=typd
+
+		$getfield = '?q='. $searchTerm . '&result_type=' . $result_type .'&count=' . $count . '&src=typd';
+		$requestMethod = 'GET';
+
+		$twitter = new TwitterAPIExchange($settings);
+		$jsonResponse =  $twitter->setGetfield($getfield)
+			                     ->buildOauth($url, $requestMethod)
+			                     ->performRequest();
+		$results = json_decode($jsonResponse);
+		dd($results);
+
+
+		$alchemyapi = new AlchemyAPI();
+		$options = ['sentiment' => 1];
+
+//		dd($response);
+
+
+
+		$tweets = [];
+		$concepts = [];
+		$keywords = [];
+		$entities = [];
+
+		foreach($results->statuses as $tweet)
+		{
+//			dd($tweet);
+			
+			var_dump($tweet->text);
+			//push all of the following to their own respective 4 arrays
+			//search through concepts, keywords, and entities to aggregate results
+			//average relevance and sentiments
+
+			//make sure to grab linked data? link to in table?
+//			$response = $alchemyapi->concepts('text', $tweet->text, $options);
+//			$response = $alchemyapi->keywords('text', $tweet->text, $options);
+//			$response = $alchemyapi->sentiment('text', $tweet->text, $options);
+			$response = $alchemyapi->entities('text', $tweet->text, $options);
+			dd($response);
+
+
+//			array_push($tweets, $tweet);
+			//feed tweets into alchemy api sentiment
+
+		}
+
+		dd($results);
 
 	}
 
--- laravel/app/views/home.blade.php
+++ laravel/app/views/home.blade.php
@@ -1,10 +1,13 @@
 @extends('master')
 @section('content')
-	<!--twitterSearch Form Input-->
-	<div class="form-group">
-	    {{ Form::label('twitterSearch', 'Twitter Search: ') }}
-	    {{ Form::input('text', 'twitterSearch', null, array('class' => 'form-control')) }}
-	</div>
 
+	{{ Form::open([ 'action' => 'TwitterController@search', 'class' => 'clearfix', 'style' => 'padding:1em 3em;']) }}
+		<!--twitterSearch Form Input-->
+		<div class="form-group">
+			{{ Form::label('twitterSearch', 'Twitter Search: ') }}
+			{{ Form::input('text', 'twitterSearch', null, array('class' => 'form-control')) }}
+		</div>
+		<button class="btn btn-success" type="submit">Submit</button>
+	{{ Form::close() }}
 
 @endsection
