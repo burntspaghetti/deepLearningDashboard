2e78e825df42ddc99eb1c880dddd3da76b477201
singularitarian <nsbertanzetti@crimson.ua.edu>
2015-08-09 14:08:03 -0500

    refactor stackexchangecontroller search function

--- laravel/app/controllers/StackExchangeController.php
+++ laravel/app/controllers/StackExchangeController.php
@@ -22,6 +22,82 @@ class StackExchangeController extends BaseController {
             return Redirect::back()->withInput()->withErrors($validation->messages());
         }
 
+        $response = $this->getPosts(Input::get('stackExchangeSearch'), Input::get('count'));
+
+//        function curl($url){
+//            $curl = curl_init();
+//            curl_setopt($curl, CURLOPT_URL, $url);
+//            curl_setopt($curl, CURLOPT_HEADER, 0);
+//            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//            curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+//            curl_setopt($curl, CURLOPT_USERAGENT, 'cURL');
+//            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+//            curl_setopt($curl, CURLOPT_ENCODING , "gzip");
+//
+//            $result = curl_exec($curl);
+//            curl_close($curl);
+//
+//            return $result;
+//        }
+//
+//        $searchTerm = urlencode(Input::get('stackExchangeSearch'));
+//        $count = Input::get('count');
+//
+//        $searchURL = "http://api.stackexchange.com/2.2/search?pagesize=". $count . "&order=desc&sort=activity&intitle=". $searchTerm ."&site=stackoverflow";
+//        $response = json_decode(curl($searchURL));
+
+        $alchemyapi = new AlchemyAPI();
+        $postIntel = [];
+
+        foreach($response->items as $post)
+        {
+            $textOptions = ['useMetadata' => 1];
+            $textResponse = $alchemyapi->text('url', $post->link, $textOptions);
+
+            $sentimentOptions = ['sentiment' => 1];
+            $postSentiment = $this->doAlchemy('url', $post->link, $sentimentOptions, 'sentiment', 'docSentiment', $alchemyapi);
+
+            //Dont use concepts
+            //when loading stack url into concepts call, it just returns random unrelated stuff
+            //when loading extracted text into concepts call, it returns empty every time
+
+            $entityOptions = ['maxRetrieve' => 5, 'sentiment' => 1];
+            $postEntities = $this->doAlchemy('text', $textResponse['text'], $entityOptions, 'entities', 'entities', $alchemyapi);
+
+            $keywordOptions = ['sentiment' => 1, 'showSourceText' => 1, 'maxRetrieve' => 5];
+            $postKeywords = $this->doAlchemy('text', $textResponse['text'], $keywordOptions, 'keywords', 'keywords', $alchemyapi);
+
+            $tmp = ['link' => $post->link,
+                    'title' => $post->title,
+                    'answered' => $post->is_answered,
+                    'sentiment' => $postSentiment,
+                    'entities' => $postEntities,
+                    'keywords' => $postKeywords];
+            
+            array_push($postIntel, $tmp);
+        }
+
+        return View::make('stackResults')->with('postIntel', $postIntel)->with('searchTerm', Input::get('stackExchangeSearch'));
+	}
+
+
+    private function doAlchemy($flavor, $data, $options, $call, $key, $alchemyapi)
+    {
+        $response = $alchemyapi->$call($flavor, $data, $options);
+        if(array_key_exists($key, $response))
+        {
+            $intel = $response[$key];
+        }
+        else
+        {
+            $intel = null;
+        }
+
+        return $intel;
+    }
+
+    private function getPosts($searchTerm, $count)
+    {
         function curl($url){
             $curl = curl_init();
             curl_setopt($curl, CURLOPT_URL, $url);
@@ -37,83 +113,10 @@ class StackExchangeController extends BaseController {
 
             return $result;
         }
-        $searchTerm = urlencode(Input::get('stackExchangeSearch'));
-        $count = Input::get('count');
 
         $searchURL = "http://api.stackexchange.com/2.2/search?pagesize=". $count . "&order=desc&sort=activity&intitle=". $searchTerm ."&site=stackoverflow";
         $response = json_decode(curl($searchURL));
 
-
-        $alchemyapi = new AlchemyAPI();
-        $postIntel = [];
-
-        foreach($response->items as $post)
-        {
-            $textOptions = ['useMetadata' => 1];
-            $textResponse = $alchemyapi->text('url', $post->link, $textOptions);
-
-            //DONE
-            $sentimentOptions = ['sentiment' => 1];
-//            $sentimentResponse = $alchemyapi->sentiment('text', $textResponse['text'], $sentimentOptions);
-            $sentimentResponse = $alchemyapi->sentiment('url', $post->link, $sentimentOptions);
-            if(array_key_exists('docSentiment', $sentimentResponse))
-            {
-                $postSentiment = $sentimentResponse['docSentiment'];
-            }
-            else
-            {
-                $postSentiment = null;
-            }
-
-            //DONT USE
-            //when loading stack url into concepts call, it just returns random unrelated stuff
-            //when loading extracted text into concepts call, it returns empty every time
-//            $conceptOptions = ['maxRetrieve' => 5, 'linkedData' => 1, 'showSourceText' => 1];
-//            $conceptsResponse = $alchemyapi->concepts('text', $textResponse, $conceptOptions);
-//
-//            if(array_key_exists('concepts', $conceptsResponse))
-//            {
-//                $postConcepts = $conceptsResponse['concepts'];
-//                array_push($concepts, $postConcepts);
-//            }
-
-
-            $entityOptions = ['maxRetrieve' => 5, 'sentiment' => 1];
-//            $entityResponse = $alchemyapi->entities('url', $post->link, $entityOptions);
-            //text seems to return better results than link?
-            $entityResponse = $alchemyapi->entities('text', $textResponse['text'], $entityOptions);
-            if(array_key_exists('entities', $entityResponse))
-            {
-                $postEntities = $entityResponse['entities'];
-            }
-            else
-            {
-                $postEntities = null;
-            }
-
-
-            $keywordOptions = ['sentiment' => 1, 'showSourceText' => 1, 'maxRetrieve' => 5];
-//            $keywordResponse = $alchemyapi->keywords('url', $post->link, $keywordOptions);
-            $keywordResponse = $alchemyapi->keywords('text', $textResponse['text'], $keywordOptions);
-            if(array_key_exists('keywords', $keywordResponse))
-            {
-                $postKeywords = $keywordResponse['keywords'];
-            }
-            else
-            {
-                $postKeywords = null;
-            }
-
-            $tmp = ['link' => $post->link,
-                    'title' => $post->title,
-                    'answered' => $post->is_answered,
-                    'sentiment' => $postSentiment,
-                    'entities' => $postEntities,
-                    'keywords' => $postKeywords];
-            
-            array_push($postIntel, $tmp);
-        }
-
-        return View::make('stackResults')->with('postIntel', $postIntel)->with('searchTerm', Input::get('stackExchangeSearch'));
-	}
+        return $response;
+    }
 }
